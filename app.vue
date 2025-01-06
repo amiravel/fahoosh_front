5bac5e40fa0f448d08b7dae828295852a378a242
amiravel <amirhadi747@gmail.com>
2025-01-06 18:06:34 +0330

    list of rooms are available

--- app.vue
+++ app.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+
+import Nav from "~/components/Nav.vue";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-gray-400">
+    <div class="w-10/12 mx-auto min-h-screen">
+      <NuxtRouteAnnouncer/>
+
+      <NuxtLayout>
+        <Nav></Nav>
+        <div class="mt-5 bg-white p-5 rounded shadow">
+
+          <NuxtPage></NuxtPage>
+        </div>
+      </NuxtLayout>
+    </div>
   </div>
+
 </template>
+
+<style scoped>
+
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+
+    <nav class="bg-white p-3 rounded shadow">
+      <ul class="flex flex-row items-center">
+        <li class="mr-4">
+          <a href="/">Home</a>
+        </li>
+        <li class="mr-4">
+          <a href="/rooms">Rooms</a>
+        </li>
+        <li class="mr-4">
+          <a href="/users">Users</a>
+        </li>
+        <li class="mr-4">
+          <a href="/reserves">Reserves</a>
+        </li>
+      </ul>
+    </nav>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
